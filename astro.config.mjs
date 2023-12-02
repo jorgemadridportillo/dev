f7c612477705c7d7b6192f1ada1f2aa3f425ded4
Jorge <jorgemadridportillo@gmail.com>
2023-12-03 00:19:03 +0100

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jorgemadridportillo.github.io',
+  base: '/jorgemadrid-dev-web',
   integrations: [tailwind()]
-});
+})
