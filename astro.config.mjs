3d900812b5aade63cfc640df86d10b83f7d3d5f2
Jorge <jorgemadridportillo@gmail.com>
2023-12-10 22:34:07 +0100

    feat(astro.config.mjs): change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jorgemadridportillo.github.io',
-  base: '/jorgemadrid-dev-web',
+  base: '/dev',
   integrations: [tailwind()]
 })
